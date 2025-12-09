e55b825db921313846bbe6a5f1ed92dd57687765
Pierre Leroux <p.leroux@gmx.com>
2025-12-09 22:01:51 +0100

    fix: forget to replace all secrets.json import

--- front-end/src/model/git/init.ts
+++ front-end/src/model/git/init.ts
@@ -5,12 +5,11 @@ import * as fsp from "node:fs/promises";
 import * as fs from "node:fs";
 import { gitBranch, gitCheckout, gitClone, gitInstance, gitListRemote, gitPull, gitRemoteShowOrigin, gitShowRefBranch, gitUpdateRemoteOrigin } from './cmd.js';
 import type { IAuthentication } from '../cookie.js';
-
-import * as secrets from "../../secrets.json" with { type: "json" };
+import { secrets } from '../secrets.js';
 
 export const gitInit = async (auth: IAuthentication, sessionId: string, expiresAt: string, mode: "dev"|"prod" = "dev"): Promise<{ git: SimpleGit, expiresAt: string, gitbaseDir: string, branchName: string }> => {
 
-    const user = secrets.default.users.find(({ id }) => auth.id === id);
+    const user = secrets.users.find(({ id }) => auth.id === id);
     if (!user) {
         throw new Error("No User Found !");
     }
