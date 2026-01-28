cf4993e10f539cb52cee9171ca84a6fe1630a55f
Pierre Leroux <p.leroux@gmx.com>
2026-01-28 14:08:08 +0100

    chore: delete front-end directory

--- front-end/scripts/docker-build.sh
+++
@@ -1,8 +0,0 @@
-
-set -xe
-
-# docker build -f dockerfile.node -t europe-west1-docker.pkg.dev/customization-profile/cloud-run-front-end-build/front-end-node:0.0.0.test --progress plain .
-
-# docker build -f dockerfile.deno -t europe-west1-docker.pkg.dev/customization-profile/cloud-run-front-end-build/front-end-deno:0.0.0.test --progress plain .
-
-docker build -f dockerfile.bun -t europe-west1-docker.pkg.dev/customization-profile/cloud-run-front-end-build/front-end-bun:0.0.0.test --progress plain .
